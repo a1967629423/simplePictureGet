5e635bb93a6280672b9d44b348c21e8ff2eaaa67
deng zi wen <1967629423@qq.com>
2019-07-07 00:37:11 +0800

    test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as http from 'http'
+import express from 'express';
 const myApp = http.createServer((req,res)=>{
     res.writeHead(200);
     res.end('Hello')
 });
+
 myApp.listen(9996,'localhost')
